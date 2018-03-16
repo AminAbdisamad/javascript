a0e1b23ae65b7ab88280d6c8060df69372ec0b9d
Amin Abdisamad <theaminux@gmail.com>
2018-03-16 22:20:28 +0300

    Initial commit

---
+++ DOM/main.js
@@ -0,0 +1,20 @@
+let val;
+val = document.all[8];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.textContent;
+val = document.charset;
+val = document.contentType;
+
+val= document.forms;
+val = document.forms[0].id;
+val = document.forms[0].method;
+val = document.forms[0].action;
+
+val = document.links[0];
+val = document.links[0].className;
+console.log(val);
