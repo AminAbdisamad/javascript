a3be2bafae59637485d4978400144b311986f224
Amin Abdisamad <theaminux@gmail.com>
2018-03-17 20:48:23 +0300

    updated

--- DOM/main.js
+++ DOM/main.js
@@ -60,8 +60,8 @@ document.querySelector('li:nth-child(5)').style.color='orange';
 document.querySelector('li').style.color='red';
 document.querySelector('.btn').style.color="white";
 document.querySelector('a:nth-child(1)').style.color="red";
-document.querySelector('li:nth-child(odd)').style.background='#ccc';
-document.querySelector('li:nth-child(even)').style.background='#f4f4f4';
+//document.querySelector('li:nth-child(odd)').style.background='#ccc';
+//document.querySelector('li:nth-child(even)').style.background='#f4f4f4';
 
 const collectionItem = document.querySelector('.collection-item');
 
@@ -84,3 +84,73 @@ const collectionItem = document.querySelector('.collection-item');
 // testCollection.style.textAlign ="center";
 // testCollection.style.margin = '0 25px';
 
+
+/* HTML collectioin is not an array  
+CONVERTING HTML COLLECTION INTO ARRAY
+*/
+let listItem = document.getElementsByTagName('li');
+
+listItem = Array.from(listItem); //converting HTML collection to array
+//listItem.reverse();
+listItem.forEach(function(list,index){
+    //console.log(list);
+    //list.textContent = `${index} - I will creat Aminux OS IA`;
+})
+//console.log(listItem);
+
+/* QuerySelectorALL - returns nodelist
+it allows us to use with forEach without converting it*/
+//TODO - 1. select multiple elements 2. Loop through multiple elements 3. change color for odd and even lists (use two different loops)
+
+const theMList = document.querySelectorAll('.collection .collection-item');
+//console.log(theMList);
+//theMList.forEach(item => console.log(item));
+
+//odd list background using forEach
+const listOdd = document.querySelectorAll('.collection-item:nth-child(odd)');
+listOdd.forEach(oddlist =>oddlist.style.background='#fff');
+//evenlist background using forloop
+const listEven = document.querySelectorAll('.collection-item:nth-child(even)');
+for(let even = 0; even < listEven.length; even++){
+    listEven[even].style.background="#fff";
+}
+
+//while loop
+// let even = 0;
+// while(even < listEven.length){
+//     listEven[even].style.background="#f03";
+//     even++;
+    
+// }
+
+/* -------------------
+ TRAVERSING THE DOM 
+ --------------------*/
+
+ const theListCol = document.querySelector('ul.collection');
+ const listItems = document.querySelector('li.collection-item:first-child');
+
+ /* Get Child Nodes */
+
+val = theListCol.childNodes;
+val = theListCol.childNodes[0];
+val = theListCol.childNodes[1];
+val = theListCol.childNodes[2].nodeType;
+val = theListCol.childNodes[2].nodeName;
+
+/* Node Type Numbers 
+--------------------
+1. Element
+2. Attribute (deprecated)
+3.Text node
+8.Comment
+9.Document itself
+10.Doctype*/
+
+/* -------------------
+ Get children 
+ --------------------*/
+ val = theListCol.children;
+ val = theListCol.children[3];
+
+console.log(val);
