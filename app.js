ec676888b61a0ec42a5d88fbee879f5bcfb10c07
Amin Abdisamad <theaminux@gmail.com>
2018-08-17 20:45:12 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
+// Create posts
+const posts = [
+  { title: 'Post One', body: 'This is post one' },
+  { title: 'Post Two', body: 'This is post two' }
+];
 
-navigator.geolocation.getCurrentPosition(function(location) {
-    console.log(location.coords.latitude);
-    console.log(location.coords.longitude);
-    console.log(location.coords.accuracy);
-  }); 
+// create post
+function createPosts(post) {
+  setTimeout(() => {
+    posts.push(post);
+  }, 2000);
+}
+
+// Get posts
+function getPost() {
+  setTimeout(() => {
+    let output = '';
+    posts.forEach(function(post) {
+      output += `
+        <li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+createPosts({ title: 'Post Three', body: 'This is post Three' });
+getPost();
+
+function createPosts(post, callback) {
+  setTimeout(() => {
+    posts.push(post);
+    callback();
+  }, 2000);
+}
+
+function getPosts() {
+  setTimeout(() => {
+    let output = '';
+    posts.forEach(post => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+createPosts({ title: 'Post Three', body: 'This is Post Three' }, getPosts);
+// getPosts();
